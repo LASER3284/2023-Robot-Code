f3aef7b129c22d146db04318a3cae17b9f50853b
NateFranklin <98236860+NateFranklin@users.noreply.github.com>
2023-01-27 15:05:10 -0600

    Update vendordeps / add basic intake class

---
+++ src/main/cpp/Intake.cpp
@@ -0,0 +1,29 @@
+#include "Intake.h"
+
+intake::Intake::Intake() {
+    // TODO: Setup the NEO 550
+    //       - Current Limits (20A)
+    //       - 
+}
+
+void intake::Intake::ConeMode(){
+    
+}
+
+void intake::Intake::CubeMode() { 
+
+}
+
+void intake::Intake::Spin() {
+
+}
+
+void intake::Intake::Spit() {
+
+}
+
+bool intake::Intake::HasElement() {
+    // TODO: Implement current spike detection (?)
+    return false;
+}
+
---
+++ src/main/include/Intake.h
@@ -0,0 +1,28 @@
+// this code is to create the classes for the intake and crate the numatc controls of the intake 
+#include "Robot.h"
+#include "Constants.h"
+
+#include <rev/CANSparkMax.h>
+
+namespace intake {
+    class Constants {
+        static constexpr int kIntakeID = 10;
+    };
+
+    class Intake {
+        public:
+            Intake();
+
+            void ConeMode();
+            void CubeMode();
+
+            void Spin();
+            void Spit();
+
+            // TODO: Implement
+            bool HasElement();
+        private:
+            rev::CANSparkMax intakeMotor { kIntakeID, rev::CANSparkMaxLowLevel::MotorType::kBrushless };  
+    };
+    
+}
