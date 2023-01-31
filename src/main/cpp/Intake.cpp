2c682ca980a75942cdbe8cb7c6e34cb134ad7ab8
NateFranklin <98236860+NateFranklin@users.noreply.github.com>
2023-01-31 15:34:23 -0600

    Added functions to intake so we can move it.

--- src/main/cpp/Intake.cpp
+++ src/main/cpp/Intake.cpp
@@ -1,29 +1,29 @@
 #include "Intake.h"
 
 intake::Intake::Intake() {
-    // TODO: Setup the NEO 550
-    //       - Current Limits (20A)
-    //       - 
+    intakeMotor.SetSmartCurrentLimit(25);
 }
 
 void intake::Intake::ConeMode(){
-    
+    intakeSolenoid1.Set(true);
+    intakeSolenoid2.Set(true);
 }
 
 void intake::Intake::CubeMode() { 
-
+    intakeSolenoid1.Set(false);
+    intakeSolenoid2.Set(false);
 }
 
-void intake::Intake::Spin() {
-
+void intake::Intake::Inhale() {
+    intakeMotor.Set(0.25);
 }
 
 void intake::Intake::Spit() {
-
+    intakeMotor.Set(-0.25);
 }
 
 bool intake::Intake::HasElement() {
     // TODO: Implement current spike detection (?)
-    return false;
+    return GetFilteredCurrent() >= 10_A;
 }
 
--- src/main/include/Intake.h
+++ src/main/include/Intake.h
@@ -3,11 +3,14 @@
 #include "Constants.h"
 
 #include <rev/CANSparkMax.h>
+#include <frc/Solenoid.h>
+#include <frc/filter/LinearFilter.h>
 
 namespace intake {
     class Constants {
         public:
             static constexpr int kIntakeID = 10;
+        
     };
 
     class Intake {
@@ -17,13 +20,23 @@ namespace intake {
             void ConeMode();
             void CubeMode();
 
-            void Spin();
+            void Inhale();
             void Spit();
+
+            units::ampere_t GetFilteredCurrent() {
+                return highPass.Calculate(movingAverage.Calculate(units::ampere_t{intakeMotor.GetOutputCurrent()}));
+            }
+
 
             // TODO: Implement
             bool HasElement();
         private:
-            rev::CANSparkMax intakeMotor { Constants::kIntakeID, rev::CANSparkMaxLowLevel::MotorType::kBrushless };  
+            rev::CANSparkMax intakeMotor { Constants::kIntakeID, rev::CANSparkMaxLowLevel::MotorType::kBrushless };
+            frc::Solenoid intakeSolenoid1 { frc::PneumaticsModuleType::REVPH, 1 };
+            frc::Solenoid intakeSolenoid2 { frc::PneumaticsModuleType::REVPH, 2 };
+
+            frc::LinearFilter<units::ampere_t> movingAverage = frc::LinearFilter<units::ampere_t>::MovingAverage(8);
+            frc::LinearFilter<units::ampere_t> highPass = frc::LinearFilter<units::ampere_t>::HighPass(0.5, 0.02_s);
+
     };
-    
 }
